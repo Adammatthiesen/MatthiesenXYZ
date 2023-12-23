533690ea2d75b7fb869483121f3d5b1e7e25cd80
Adam Matthiesen <amatthiesen@outlook.com>
2023-12-22 23:37:43 -0800

    modified:   astro.config.mjs 	modified:   package.json 	new file:   src/pages/contact.astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,12 @@ import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import tailwind from "@astrojs/tailwind";
 import { remarkModifiedTime } from './remark-modified-time.mjs';
+import cloudflare from '@astrojs/cloudflare';
 
 // https://astro.build/config
 export default defineConfig({
+  output: 'server',
+  adapter: cloudflare(),
   site: 'https://matthiesen.xyz',
   integrations: [mdx(), sitemap(), tailwind()],
   markdown: {
