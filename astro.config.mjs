be038607b13bcc5c973f109d13bbf69f9f805ca0
Adam Matthiesen <amatthiesen@outlook.com>
2023-12-24 14:47:44 -0800

    test upload search feature

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,6 @@ import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import tailwind from "@astrojs/tailwind";
 import { remarkModifiedTime } from './remark-modified-time.mjs';
-import cloudflare from '@astrojs/cloudflare';
 
 // https://astro.build/config
 export default defineConfig({
